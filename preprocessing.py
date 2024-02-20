b34b3bc5b6cdb33d032bf4121efc54608ac51c07
amblcw <amblcw@naver.com>
2024-02-20 16:58:05 +0900

    make trasfer function

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import pickle
+import os.path
 
 def make_passenger_csv():
     ''' 
@@ -29,6 +31,9 @@ def make_transfer_csv():
     각 역별 환승(버스에서 지하철로 추정) 이용객 수 데이터를 만드는 함수입니다
     각종 이용객 유형을 제거하고 단순 이용객 수로 합쳤습니다
     '''
+    if os.path.exists('./data/trasfer_list.pkl'):           # 이미 존재하면 있는 파일 읽어서 반환
+        return pd.read_pickle('./data/trasfer_list.pkl')
+    
     row_transfer_csv = pd.read_csv("./data/서울교통공사_1_8호선 역별 일별 승객유형별 수송인원(환승유입인원 포함) 정보_20221231.csv",index_col=0,encoding="EUC-KR")
     subway_line_num = row_transfer_csv['호선']
     subway_line_num = subway_line_num.str.replace("호선","").astype(int)    # 몇호선에서 숫자만 남김
@@ -38,7 +43,6 @@ def make_transfer_csv():
     station_list = np.unique(row_transfer_csv['역번호'])
     
     transfer_list = pd.DataFrame()
-    data_list = []
     for date in date_list:
         split_by_date = row_transfer_csv[row_transfer_csv["날짜"] == date].copy()
         for subway_line in subway_line_list:
@@ -47,9 +51,10 @@ def make_transfer_csv():
                 temp_data = split_by_line[split_by_line["역번호"] == station_num].copy()
                 if temp_data.shape[0] == 0:
                     continue
-                # data = pd.DataFrame({'날짜':[date],'호선':[subway_line],'역번호':[station_num],'환승유입인원':[temp_data['환승유입인원'].sum()]})
-                # transfer_list = pd.concat([transfer_list,data])
-        
+                data = pd.DataFrame({'날짜':[date],'호선':[subway_line],'역번호':[station_num],'환승유입인원':[temp_data['환승유입인원'].sum()]})
+                transfer_list = pd.concat([transfer_list,data])
+
+    transfer_list.to_pickle('./data/trasfer_list.pkl')
     return transfer_list
 
 def label_encode():
