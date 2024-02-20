81a41e6c7a9de1eb120d152eb80e4ca8a7e31c82
amblcw <amblcw@naver.com>
2024-02-20 17:23:06 +0900

    preprocessing fit

--- preprocessing.py
+++ preprocessing.py
@@ -28,15 +28,13 @@ def make_transfer_csv():
     from: 서울교통공사_1_8호선 역별 일별 승객유형별 수송인원(환승유입인원 포함) 정보_20221231.csv
     return: pd.Dataframe
     
-    각 역별 환승(버스에서 지하철로 추정) 이용객 수 데이터를 만드는 함수입니다
+    각 역별 환승(버스에서 지하철로 추정) 이용객 수 데이터를 만드는 함수입니다(라벨인코딩 포함)
     각종 이용객 유형을 제거하고 단순 이용객 수로 합쳤습니다
     '''
     if os.path.exists('./data/trasfer_list.pkl'):           # 이미 존재하면 있는 파일 읽어서 반환
         return pd.read_pickle('./data/trasfer_list.pkl')
     
     row_transfer_csv = pd.read_csv("./data/서울교통공사_1_8호선 역별 일별 승객유형별 수송인원(환승유입인원 포함) 정보_20221231.csv",index_col=0,encoding="EUC-KR")
-    subway_line_num = row_transfer_csv['호선']
-    subway_line_num = subway_line_num.str.replace("호선","").astype(int)    # 몇호선에서 숫자만 남김
     
     date_list = np.unique(row_transfer_csv['날짜'])
     subway_line_list = np.unique(row_transfer_csv['호선'])
@@ -54,6 +52,9 @@ def make_transfer_csv():
                 data = pd.DataFrame({'날짜':[date],'호선':[subway_line],'역번호':[station_num],'환승유입인원':[temp_data['환승유입인원'].sum()]})
                 transfer_list = pd.concat([transfer_list,data])
 
+    subway_line_num = transfer_list['호선']
+    subway_line_num = subway_line_num.str.replace("호선","").astype(int)    # 몇호선에서 숫자만 남김
+    transfer_list['호선'] = subway_line_num
     transfer_list.to_pickle('./data/trasfer_list.pkl')
     return transfer_list
 
